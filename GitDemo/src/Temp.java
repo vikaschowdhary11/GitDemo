b484bec61a311843d72ab090f3305ad34aa6c3de
Vikas Chowdhary <vikaschowdhary11@gmail.com>
2018-03-18 12:14:55 +0530

    Fixed greeting dispalyed

--- GitDemo/src/Temp.java
+++ GitDemo/src/Temp.java
@@ -4,8 +4,8 @@ public class Temp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("World of Github!!!! Welcome");
-		System.out.println("Git Demo project created");
+		System.out.println("World of Github!!!! Welcome".toUpperCase());
+		System.out.println("Git Demo project created".toUpperCase());
 		
 		//Print data
 				int arr[]= {1,2,3};
