deba9875a1c6d1e99d1b5a13bfd32da91b807f67
Vikas Chowdhary <vikaschowdhary11@gmail.com>
2018-03-18 12:09:51 +0530

    Array data is added

--- GitDemo/src/Temp.java
+++ GitDemo/src/Temp.java
@@ -6,6 +6,12 @@ public class Temp {
 
 		System.out.println("World of Github!!!! Welcome");
 		System.out.println("Git Demo project created");
+		
+		//Print data
+				int arr[]= {1,2,3};
+				
+				for(int a:arr)
+					System.out.println(a);
 	}
 
 }
